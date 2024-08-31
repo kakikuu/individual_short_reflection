5e48f62aff9786b0c5fb585cd2330c3ee945fee9
Tono <s222055@stu.musashino-u.ac.jp>
2024-08-31 09:56:38 +0900

    #2 memosテーブルの修正

--- database/migration/V2__create_memos.sql
+++ database/migration/V2__create_memos.sql
@@ -2,7 +2,8 @@ CREATE memos (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
     title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
+    what_miss TEXT NOT NULL,
+    why_miss TEXT NOT NULL,
+    prevent_miss TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
