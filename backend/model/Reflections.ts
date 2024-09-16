500287ad7b5cca157fe8506fabf9b902deb1ef5c
Tono <s222055@stu.musashino-u.ac.jp>
2024-09-16 15:38:01 +0900

    add:reflectionのmodel作成

--- backend/model/Reflections.ts
+++ backend/model/Reflections.ts
@@ -1,9 +1,33 @@
-interface Reflections {
-  id: string;
-  user_id: string;
-  title: string;
-  what_miss: string;
-  why_miss: string;
-  prevent_miss: string;
-  created_at: Date;
+import { createClient } from "@supabase/supabase-js";
+import type { getReflections } from "../types/reflection";
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+  throw new Error("Missing SUPABASE_URL or SUPABASE_ANON_KEY in env");
 }
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+// getする関数
+export const getReflectionByReflectionId = async (reflectionId: string) => {
+  const { data, error } = await supabase
+    .from("reflections")
+    .select("*")
+    .eq("id", reflectionId);
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+// insertする関数
+export const createReflection = async (reflection: createReflection) => {
+  const { data, error } = await supabase
+    .from("reflections")
+    .insert([reflection]);
+  if (error) {
+    throw error;
+  }
+  return data;
+};
