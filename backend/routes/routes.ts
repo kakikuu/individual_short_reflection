56dff6a92defe386fe8eadbc968e0c8f60b74e5e
Tono <s222055@stu.musashino-u.ac.jp>
2024-09-23 14:12:54 +0900

    add:corsの処理

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -8,9 +8,21 @@ import {
   signupController,
   loginController,
 } from "../controllers/UserControllers";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+
+// 必要に応じて詳細なオプションを追加
+app.use(
+  cors({
+    origin: "*", // すべてのオリジンを許可、特定のドメインを指定することも可能
+    methods: "GET,POST,PUT,DELETE", // 許可するHTTPメソッド
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // JSON形式のリクエストボディをパースするためのミドルウェア
 app.use(express.json());
