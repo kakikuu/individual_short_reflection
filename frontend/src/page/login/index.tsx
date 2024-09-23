6cc03a86e62954cff2f7e5f7b4ebf865083f2dc5
Tono <s222055@stu.musashino-u.ac.jp>
2024-09-23 14:12:41 +0900

    add:loginの処理

--- frontend/src/page/login/index.tsx
+++ frontend/src/page/login/index.tsx
@@ -1,16 +1,51 @@
+import { useState } from "react";
 export const LoginPage = () => {
-  const handleSubmit = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeUsername = (e: any) => {
+    setUsername(e.target.value);
+  };
+
+  const changePassword = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  //   どこでsetするか問題
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
     console.log("投稿しました");
+
+    // TODO: Tanstackqueryを使用する
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <>
       <h1>ログイン</h1>
       <form action="">
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="text" placeholder="username" onChange={changeUsername} />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={changePassword}
+        />
         <button type="submit" onClick={handleSubmit}>
-          {" "}
-          ログイン{" "}
+          ログイン
         </button>
       </form>
     </>
