50f1dbeff8c5b40d5c6fd3cc48b586aaacf2058d
Tono <s222055@stu.musashino-u.ac.jp>
2024-09-29 20:18:55 +0900

    add:基本画面の作成

---
+++ frontend/src/app/page/signup/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const changeUsername = (e: any) => {
+    setUsername(e.target.value);
+  };
+
+  const changePassword = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setErrorMessage("");
+    // ここの関数を登録する関数に変える
+    const result = await fetchUserData({ username, password });
+    if (result.success) {
+      navigate("/home");
+    } else {
+      setErrorMessage(result.message);
+    }
+  };
+  return (
+    <>
+      <h1>サインアップ</h1>
+      {errorMessage && <p>すでに存在するusernameです。</p>}
+      <form action="">
+        {}
+        <input type="text" placeholder="username" onChange={changeUsername} />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={changePassword}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          サインアップ
+        </button>
+      </form>
+    </>
+  );
+};
