a8b08ba194759afa78dac8d0f311dc6c725f39a4
Tono <s222055@stu.musashino-u.ac.jp>
2024-10-11 23:01:44 +0900

    add:reflection表示

--- frontend/src/app/page/view/index.tsx
+++ frontend/src/app/page/view/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../../../context/AuthContext";
 import { useEffect } from "react";
+import { useState } from "react";
 import { fetchAReflection } from "../../../client/reflection";
+import type { Reflection } from "../../../types/reflection";
 
 export const ViewReflectionPage = () => {
-  // paramでreflection_idを取得
+  const [reflection, setReflection] = useState([] as Reflection[]);
   const { reflection_id_string } = useParams();
   const reflection_id = Number(reflection_id_string);
 
@@ -14,6 +16,11 @@ export const ViewReflectionPage = () => {
   const fetchReflection = async (userId: string, reflection_id: number) => {
     if (!reflection_id || !userId) return;
     const result = await fetchAReflection(userId, reflection_id);
+    if (!result.success) {
+      console.log("エラー");
+      return;
+    }
+    setReflection(result.data);
     console.log(result);
   };
 
@@ -25,6 +32,18 @@ export const ViewReflectionPage = () => {
   return (
     <div>
       <h1>View Reflection</h1>
+      {reflection && (
+        <div>
+          {reflection.map((reflection, index) => (
+            <div key={index}>
+              <h2>{reflection.title}</h2>
+              <p>{reflection.whatMiss}</p>
+              <p>{reflection.whyMiss}</p>
+              <p>{reflection.preventMiss}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
