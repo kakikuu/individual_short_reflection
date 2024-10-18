a83640e03227cef1cc2bb859bcaef0ad60233637
Tono <s222055@stu.musashino-u.ac.jp>
2024-10-18 23:54:43 +0900

    add:色の追加

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/***.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainYellow: "#F7C456",
+      },
+    },
   },
   plugins: [],
 };
