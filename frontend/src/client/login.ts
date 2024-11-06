34599aae1942d7bbeeedce6487f8db57aed5db6d
Tono <s222055@stu.musashino-u.ac.jp>
2024-11-06 11:18:16 +0900

    add:httpsに変更

--- frontend/src/client/login.ts
+++ frontend/src/client/login.ts
@@ -5,7 +5,7 @@ type LoginData = {
 // ログインするときの関数を定義
 export const fetchUserData = async (loginData: LoginData) => {
   try {
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://localhost:3000/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
